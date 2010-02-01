e5e0fcc27621c541a7a37bfeb8fbedda28d4f20e
kwhittingham <kwhittingham@inexas.com>
2010-02-01 21:49:29 +0100

    Snapshot

--- src/main/java/com/ecyrd/jspwiki/diff/TraditionalDiffProvider.java
+++ src/main/java/com/ecyrd/jspwiki/diff/TraditionalDiffProvider.java
@@ -1,4 +1,21 @@
 /*
+  Copyright (c) Inexas 2010
+
+  Modifications licensed under the Inexas Software License V1.0. You
+  may not use this file except in compliance with the License.
+
+  The License is available at: http://www.inexas.com/ISL-V1.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  The original file and contents are licensed under a separate license:
+  see below.
+*/
+/*
    JSPWiki - a JSP-based WikiWiki clone.
 
     Licensed to the Apache Software Foundation (ASF) under one
--- src/main/java/org/apache/catalina/util/HexUtils.java
+++ src/main/java/org/apache/catalina/util/HexUtils.java
@@ -1,4 +1,21 @@
 /*
+  Copyright (c) Inexas 2010
+
+  Modifications licensed under the Inexas Software License V1.0. You
+  may not use this file except in compliance with the License.
+
+  The License is available at: http://www.inexas.com/ISL-V1.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  The original file and contents are licensed under a separate license:
+  see below.
+*/
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
